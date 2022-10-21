a8f83733a3daa77e54627fc38601aecc5c151678
DvoraFried <013508520S@gmail.com>
2022-10-21 12:39:08 +0300

    commits

--- getEmails/getEmails.cpp
+++ getEmails/getEmails.cpp
@@ -7,20 +7,27 @@ using namespace std;
 
 int main()
 {
+	//open source file
 	ifstream source("source.txt");
+	//open 2 output files
 	ofstream csv("emails.csv");
 	ofstream txt("emails.txt");
+
 	string text;
 
 	while (!source.eof())
 	{
+		//reading line after line
 		getline(source, text);
+		
 		int start, end;
 		string email;
 
+		//Finds these characters - '<', '>' - means there is an email address between them, extracts itand outputs it to the output files
 		while (text.find("<") != string::npos && text.find(">") != string::npos) {
 			start = text.find("<");
 			end = text.find(">");
+			//copy the email
 			email = text.substr(start + 1, end - start - 1);
 			txt << email << endl;
 			csv << email << endl;
